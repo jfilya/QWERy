4629f747770e5eb940db2af0e0424561776aae0a
Julia Filippova <julia.94@bk.ru>
2023-01-10 23:01:30 +0300

    feat: add adaptive section bromo

--- qwery/src/components/Bromo/Bromo.tsx
+++ qwery/src/components/Bromo/Bromo.tsx
@@ -47,7 +47,7 @@ class Bromo extends React.Component {
             <span></span>
           </h3>
           <h2 className="bromo__title">Steady your steps, we will climb together!</h2>
-          <p  className="bromo__info">
+          <p className="bromo__info">
             Enjoying the vast expanse of the sea of ​​​​sand, witnessing the
             splendor of Mount Semeru that soars into the sky, and gazing at the
             beauty of the sun moving out of its bed or otherwise enjoying the
